64ac06fdcabb72396c86efeca1612af9d395e389
harsh <harshgolaviya@gmail.com>
2022-12-29 11:42:24 +0530

    first

--- fliesystem.js
+++ fliesystem.js
@@ -1,13 +1,13 @@
-const { Console } = require('console')
+const { Console, log } = require('console')
 const fp=require('fs')
 
 // read Asynchronously
-fp.readFile('file.txt','utf-8',(err,data)=>{
-    if(err) 
-        console.log(err)
-    else 
-        console.log(data)
-})
+// fp.readFile('file.txt','utf-8',(err,data)=>{
+//     if(err) 
+//         console.log(err)
+//     else 
+//         console.log(data)
+// })
 
 
 // read  synchronously
@@ -17,7 +17,7 @@ fp.readFile('file.txt','utf-8',(err,data)=>{
 
 // write  Asynchronously
 // dt='harsh golaviya1212'
-// fp.writeFile('js.txt',dt,'utf-8',(err)=>{
+// fp.writeFile('js11.txt',dt,'utf-8',(err)=>{
 //     if(err) 
 //         console.log(err);
 //     else  
@@ -29,6 +29,40 @@ fp.readFile('file.txt','utf-8',(err,data)=>{
 // dt='harsh golaviya1221212'
 // data=fp.writeFileSync('file.txt',dt,'utf-8')
 
+
+
+// rename Asynchronously 
+// fp.rename('file22.txt','file.txt',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('successfully...');
+// })
+
+
+// rename synchronously 
+// fp.renameSync('file.sql','file33.txt')
+
+
+// unlink(delete file) Asynchronously 
+// fp.unlink('js11.txt',(err)=>{
+//     if(err)
+//        console.log(err);
+//     else 
+//        console.log('successfully...');
+// })
+
+
+// unlink(delete file) synchronously 
+// fp.unlinkSync('js11.txt')
+
+
+// fp.stat('file.txt',(err,data)=>{
+//     if(err)
+//        console.log(err);
+//     else 
+//        console.log(data);
+// })
 
 
 // copy_file_Asynchronously:-
@@ -51,15 +85,12 @@ fp.readFile('file.txt','utf-8',(err,data)=>{
 //          fp.writeFileSync('file12.txt',data,'utf-8')
 // })
 
-// fp.rename
-// fp.unlink
-
 
 // Include os module and create its object
-// var os = require('os');
+var os = require('os');
   
 // // return the cpu architecture
-// console.log("CPU architecture: " + os.arch());
+console.log("CPU architecture: " , os.cpus());
   
 // // It returns the amount of free system memory in bytes
 // console.log("Free memory: " + os.freemem());
